9d30f975c530eeff617880a710ee8a44230cd075
Aribzhan <aribzankamilzanov@gmail.com>
2025-12-22 10:49:19 +0500

    new fix

--- app/services/tour_private/tour_service.py
+++ app/services/tour_private/tour_service.py
@@ -3,12 +3,23 @@ from sqlalchemy import select
 from fastapi import HTTPException
 from app.models.tours import Tours, TourCompanies
 from app.schemas.tour_schemas import TourCreate, TourUpdate
+from datetime import datetime
+
+
+def normalize_dt(dt: datetime | None):
+    if dt and dt.tzinfo:
+        return dt.replace(tzinfo=None)
+    return dt
 
 
 async def create_tour(db: AsyncSession, company: TourCompanies, data: TourCreate):
+    payload = data.model_dump()
+    payload["departure_date"] = normalize_dt(payload.get("departure_date"))
+    payload["return_date"] = normalize_dt(payload.get("return_date"))
+
     tour = Tours(
         tour_company_id=company.id,
-        **data.model_dump()
+        **payload
     )
     db.add(tour)
     await db.commit()
@@ -18,9 +29,7 @@ async def create_tour(db: AsyncSession, company: TourCompanies, data: TourCreate
 
 async def get_tours(db: AsyncSession, company: TourCompanies):
     r = await db.execute(
-        select(Tours).where(
-            Tours.tour_company_id == company.id
-        )
+        select(Tours).where(Tours.tour_company_id == company.id)
     )
     return r.scalars().all()
 
@@ -42,7 +51,15 @@ async def update_tour(
     if not tour:
         raise HTTPException(404, "Tour not found")
 
-    for k, v in data.model_dump(exclude_unset=True).items():
+    payload = data.model_dump(exclude_unset=True)
+
+    if "departure_date" in payload:
+        payload["departure_date"] = normalize_dt(payload["departure_date"])
+
+    if "return_date" in payload:
+        payload["return_date"] = normalize_dt(payload["return_date"])
+
+    for k, v in payload.items():
         setattr(tour, k, v)
 
     await db.commit()
