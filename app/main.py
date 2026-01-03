88390839d1984831b522e2ae998a23c47e167068
Aribzhan <aribzankamilzanov@gmail.com>
2026-01-03 21:09:23 +0500

    new fix

--- app/main.py
+++ app/main.py
@@ -58,4 +58,4 @@ app.mount(
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", reload=True, host="127.0.0.1", port=8000)
+    uvicorn.run("app.main:app", reload=True, host="127.0.0.1", port=8080)
