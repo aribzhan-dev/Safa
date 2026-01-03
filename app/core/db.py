ffeca466a1cb3e570ca65f97f08fe99c4b6ed86e
Aribzhan <aribzankamilzanov@gmail.com>
2026-01-03 22:03:56 +0500

    new fix

--- app/core/db.py
+++ app/core/db.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
